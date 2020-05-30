b3d2eb760f7035f659c4c09011d96b91b08fc393
advaithagroup <66091183+advaithagroup@users.noreply.github.com>
2020-05-30 22:14:28 +0530

    Update main.js

--- codefiles/js/main.js
+++ codefiles/js/main.js
@@ -45,6 +45,7 @@ function items_loaded(){
       settings: settings,
       cartpage: false,
       name: '',
+      phone:'',
       matches: true
     },
     computed: {
@@ -63,7 +64,7 @@ function items_loaded(){
         return n;
       },
       ordertext: function(){
-        var s = `${app.name} wants to order following Items: \n\n`;
+        var s = `${app.name} wants to order following Items: \n\n ${app.phone}`;
         for(var i=0;i<this.items.length;i++){
           if(this.items[i].quantity>0){
             s += `\n\n${this.items[i].Name}\nQuantity: ${this.items[i].quantity}`
