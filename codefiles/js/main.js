1c3e80f2cf79864b735690273fc10615ff03879d
advaithagroup <66091183+advaithagroup@users.noreply.github.com>
2020-06-23 23:17:08 +0530

    Update main.js

--- codefiles/js/main.js
+++ codefiles/js/main.js
@@ -7,7 +7,7 @@ let settings = {
   currency: '₹',
   shopphone: '8147169569',
   shopemail: 'None',
-  inventoryURL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1rYmGwL8X7goUgaajFOaIPUk3qmNR1iM9BZCgd6X18uwjdDmaH6r9n71n0mphQbKOcStkaCzbqJFe/pubhtml'
+  inventoryURL: 'https://docs.google.com/spreadsheets/d/1-1vQ1rYmGwL8X7goUgaajFOaIPUk3qmNR1iM9BZCgd6X18uwjdDmaH6r9n71n0mphQbKOcStkaCzbqJFe/pubhtml'
 };
 
 function init(){
